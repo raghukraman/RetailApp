ad1e9e7d6219a8c2b33bacc7a18fe247368af3e2
Tanveer Ahmed N M <tanveer_ahmednm@apple.com>
2016-11-22 19:13:28 +0530

    added the total functinality to both first and second screen. updated delete functionality for first and second screen. added new modify button in second screen.

--- app/src/main/java/com/retail/retailapp/ShoppingListActivity.java
+++ app/src/main/java/com/retail/retailapp/ShoppingListActivity.java
@@ -5,13 +5,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TableLayout;
@@ -19,8 +17,6 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.retail.retailapp.util.GroceryConstant;
-import com.retail.retailapp.util.GroceryUtil;
-import com.retail.retailapp.vo.GroceryItem;
 import com.retail.retailapp.vo.PurchaseItem;
 
 import java.text.DecimalFormat;
@@ -31,7 +27,8 @@ import java.util.Map;
 public class ShoppingListActivity extends AppCompatActivity {
 
     TableLayout tblLayout;
-
+    TextView totalAmountText;
+    double totalPrice = 0;
     Spinner categoryspinner, itemlistspinner, weightspinner;
 
     @Override
@@ -39,6 +36,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
         Intent caller = getIntent();
+
         Map<String, List<PurchaseItem>> data_map = (Map<String, List<PurchaseItem>>) caller.getExtras().get("shoppinglist");
         init_table_layout();
         load_table(data_map);
@@ -57,17 +55,17 @@ public class ShoppingListActivity extends AppCompatActivity {
 
                 TableRow tbrow = new TableRow(this);
 
-                CheckBox box = new CheckBox(this);
-                tbrow.addView(box);
+//                CheckBox box = new CheckBox(this);
+//                tbrow.addView(box);
 
-                TextView textView1 = new TextView(getApplicationContext());
-                textView1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-                textView1.setTextColor(Color.BLACK);
-                textView1.setText(purchaseItem.getName());
-                textView1.setPadding(1, 1, 1, 1);
+                final CheckBox checkBox = new CheckBox(this);
+                checkBox.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+                checkBox.setTextColor(Color.BLACK);
+                checkBox.setText(purchaseItem.getName());
+                checkBox.setPadding(1, 1, 1, 1);
                 Typeface tf = Typeface.createFromAsset(getAssets(), GroceryConstant.FONTS_VERDANA_TTF);
-                textView1.setTypeface(tf, Typeface.NORMAL);
-                tbrow.addView(textView1);
+                checkBox.setTypeface(tf, Typeface.NORMAL);
+                tbrow.addView(checkBox);
 
 
                 TextView tv2 = new TextView(getApplicationContext());
@@ -89,28 +87,33 @@ public class ShoppingListActivity extends AppCompatActivity {
                 tv3.setTypeface(tf, Typeface.NORMAL);
                 tbrow.addView(tv3);
 
-                Button btn = new Button(this);
-                btn.setText("X");
-//                btn.setId(rowNumber);
-                TableRow.LayoutParams tlp = new TableRow.LayoutParams(100, 52);
-                btn.setPadding(1, 0, 1, 2);
-                btn.setLayoutParams(tlp);
-
-                tbrow.addView(btn);
-
                 tblLayout.addView(tbrow);
 
-                btn.setOnClickListener(new View.OnClickListener() {
+                checkBox.setOnClickListener(new View.OnClickListener() {
 
                     @Override
                     public void onClick(View v) {
-                        // row is your row, the parent of the clicked button
-                        View row = (View) v.getParent();
-                        // container contains all the rows, you could keep a variable somewhere else to the container which you can refer to here
-                        ViewGroup container = ((ViewGroup) row.getParent());
-                        // delete the row and invalidate your view so it gets redrawn
-                        container.removeView(row);
-                        container.invalidate();
+
+                        if (checkBox.isChecked() == true) {
+                            totalAmountText = (TextView) findViewById(R.id.amount);
+                            View row = (View) v.getParent();
+                            TableRow tbRow = (TableRow) row;
+                            TextView textView = (TextView) tbRow.getChildAt(2);
+                            double rowPrice = Double.valueOf(textView.getText().toString()).doubleValue();
+                            totalPrice = totalPrice + rowPrice;
+                            totalAmountText.setText(new DecimalFormat("#.00").format(totalPrice));
+                        } else if (checkBox.isChecked() == false) {
+                            totalAmountText = (TextView) findViewById(R.id.amount);
+                            View row = (View) v.getParent();
+                            TableRow tbRow = (TableRow) row;
+                            TextView textView = (TextView) tbRow.getChildAt(2);
+                            double rowPrice = Double.valueOf(textView.getText().toString()).doubleValue();
+                            totalPrice = totalPrice - rowPrice;
+                            totalAmountText.setText(new DecimalFormat("#.00").format(totalPrice));
+                        } else {
+
+                        }
+
                     }
 
                 });
@@ -128,9 +131,9 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         Typeface tf = Typeface.createFromAsset(getAssets(), GroceryConstant.FONTS_VERDANA_TTF);
 
-
-        CheckBox box = new CheckBox(this);
-        tbrow0.addView(box);
+//
+//        CheckBox box = new CheckBox(this);
+//        tbrow0.addView(box);
 
         TextView tv1 = new TextView(this);
         tv1.setText(GroceryConstant.PRODUCT_SPACE);
@@ -152,18 +155,6 @@ public class ShoppingListActivity extends AppCompatActivity {
         tv3.setTextColor(Color.BLACK);
         tv3.setTypeface(tf, Typeface.NORMAL);
         tbrow0.addView(tv3);
-
-        Button btn = new Button(this);
-        TableRow.LayoutParams tlp = new TableRow.LayoutParams(120, 56);
-        btn.setPadding(10, 7, 0, 2);
-        btn.setGravity(Gravity.TOP);
-        btn.setLayoutParams(tlp);
-        btn.setText("X-All");
-        btn.setId(0);
-
-//        btn.setLayoutParams(layoutParams);
-        tbrow0.addView(btn);
-
 
         tblLayout.addView(tbrow0);
     }
